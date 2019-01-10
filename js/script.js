c51104635dddedde907da0158c3dd8b49f85bd3b
Shirley Gonzalez <shirleypamela1020@gmail.com>
2019-01-10 23:08:31 +0000

    First Day

--- js/script.js
+++ js/script.js
@@ -1,10 +1,15 @@
 /* global $ */
 
 // BELOW Update the songs array with four of your favorites songs.
-var songs = [];
+var songs = ["Moments","Icon","Baile de Favela","Level Up","Oye Mi Amor"];
 // BELOW Add 4 More arrays to store images_links, atists, song lengths, and links for each song
 // Make sure they match the same order as your array above
-
+var images = ["https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwj0itGJoeTfAhUyT98KHTnCDf4QjRx6BAgBEAU&url=https%3A%2F%2Fgenius.com%2FJhene-aiko-moments-lyrics&psig=AOvVaw1TkrThrVzvqS4IsZsTBFAG&ust=1547245574351539",
+"https://i.ytimg.com/vi/CFVxZFhN7lo/maxresdefault.jpg",
+"https://i.ytimg.com/vi/3n61bCFzvSo/maxresdefault.jpg",
+"https://upload.wikimedia.org/wikipedia/en/thumb/0/0d/Ciara_-_Level_Up_single_cover.png/220px-Ciara_-_Level_Up_single_cover.png",
+"https://i.ytimg.com/vi/IoOli-fR_cs/maxresdefault.jpg",
+"https://images-na.ssl-images-amazon.com/images/I/61HOSMRwkhL._SS500.jpg"];
 
 
 
